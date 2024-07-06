912c5c344220acf667126a7035b446aefbfdac00
ProDante20805 <pro.guru0114@gmail.com>
2024-07-06 04:17:38 -0700

    rerendering extension

---
+++ my-chrome-extension/popup.js
@@ -0,0 +1,5 @@
+document.getElementById('alertButton').addEventListener('click', function() {
+  const input1 = document.getElementById('input1').value;
+  const input2 = document.getElementById('input2').value;
+  alert(`Input 1: ${input1}, Input 2: ${input2}`);
+});
